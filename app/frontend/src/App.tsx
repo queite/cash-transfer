e276a3b2c3b3d163596b722bdb0aeca7ae101077
Queite <queitesc@gmail.com>
2022-11-18 19:40:35 -0300

    feat: :sparkles: adiciona app e index

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router';
+import Provider from './context/provider';
+import Home from './pages/Home';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/main' element={<Main />} />
+        </Routes>/
+      </Provider>
+
     </div>
   );
 }
